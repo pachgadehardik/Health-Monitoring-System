8ec042fa22025380b65f8f889863c475d620c72e
pachgadehardik <pachgadehardik@gmail.com>
2017-10-28 23:41:11 +0530

    Final Project (calorie)

--- app/src/main/java/com/example/hemal/hms/DBHelper.java
+++ app/src/main/java/com/example/hemal/hms/DBHelper.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
+import java.util.*;
+
 
 /**
  * Created by hemal on 16/10/17.
@@ -13,7 +15,7 @@ import android.widget.Toast;
 
 public class DBHelper extends SQLiteOpenHelper {
     public static final String DB_NAME = "User.db";
-    public static final String MAIN_TABLE =  "UserTable";
+    public static final String MAIN_TABLE = "UserTable";
     public static final String CID = "ID";
     public static final String CNAME = "FullName";
     public static final String CMAIL = "Email";
@@ -25,27 +27,27 @@ public class DBHelper extends SQLiteOpenHelper {
 
 
     public DBHelper(Context context) {
-        super(context, DB_NAME , null,1);
+        super(context, DB_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table " +MAIN_TABLE+
-                   " (ID INTEGER PRIMARY KEY AUTOINCREMENT,FULLNAME TEXT " +
-                   ", EMAIL TEXT " +
-                   ", AGE INTEGER " +
-                   ", SEX TEXT " +
-                   ", WEIGHT REAL " +
-                   ", HEIGHT REAL " +
-                   ", PWD TEXT )");
+        sqLiteDatabase.execSQL("create table " + MAIN_TABLE +
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT,FULLNAME TEXT " +
+                ", EMAIL TEXT " +
+                ", AGE INTEGER " +
+                ", SEX TEXT " +
+                ", WEIGHT REAL " +
+                ", HEIGHT REAL " +
+                ", PWD TEXT )");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("drop table if exists "+ MAIN_TABLE) ;
+        sqLiteDatabase.execSQL("drop table if exists " + MAIN_TABLE);
     }
 
-    public boolean insertData(Sign_up su,String n,String m,String p,String a,String s,String w,String h){
+    public boolean insertData(Sign_up su, String n, String m, String p, String a, String s, String w, String h) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cV = new ContentValues();
         /*String query="select * from UserTable";
@@ -57,53 +59,52 @@ public class DBHelper extends SQLiteOpenHelper {
                 Toast.makeText(su, "Full Name not entered", Toast.LENGTH_SHORT).show();
                 return false;
             }
-            if(m.length()==0) {
+            if (m.length() == 0) {
                 Toast.makeText(su, "Mail not entered", Toast.LENGTH_SHORT).show();
                 return false;
 
             }
-            if(p.length()==0) {
+            if (p.length() == 0) {
                 Toast.makeText(su, "Password not entered", Toast.LENGTH_SHORT).show();
                 return false;
             }
-            if(a.length()==0) {
+            if (a.length() == 0) {
                 Toast.makeText(su, "Age not entered", Toast.LENGTH_SHORT).show();
                 return false;
             }
-            if(s.length()==0) {
+            if (s.length() == 0) {
                 Toast.makeText(su, "Sex not entered", Toast.LENGTH_SHORT).show();
                 return false;
             }
-            if(w.length()==0) {
+            if (w.length() == 0) {
                 Toast.makeText(su, "Weight not entered", Toast.LENGTH_SHORT).show();
                 return false;
             }
-            if(h.length()==0) {
+            if (h.length() == 0) {
                 Toast.makeText(su, "Height not entered", Toast.LENGTH_SHORT).show();
                 return false;
             }
         }
-        cV.put(CNAME,n);
-        cV.put(CMAIL,m);
-        cV.put(CPWD,p);
-        cV.put(CAGE,a);
-        cV.put(CSEX,s);
-        cV.put(CWEIGHT,w);
-        cV.put(CHEIGHT,h);
-        long r = db.insert(MAIN_TABLE,null,cV);
-        if(r==-1) return false;
+        cV.put(CNAME, n);
+        cV.put(CMAIL, m);
+        cV.put(CPWD, p);
+        cV.put(CAGE, a);
+        cV.put(CSEX, s);
+        cV.put(CWEIGHT, w);
+        cV.put(CHEIGHT, h);
+        long r = db.insert(MAIN_TABLE, null, cV);
+        if (r == -1) return false;
         else return true;
     }
-    public String searchPass(String EMAIL )
-    {
+
+    public String searchPass(String EMAIL) {
         SQLiteDatabase db = this.getReadableDatabase();
-        String query="select Email,Pwd from "+ MAIN_TABLE;
-        Cursor cursor=db.rawQuery(query,null);
+        String query = "select Email,Pwd from " + MAIN_TABLE;
+        Cursor cursor = db.rawQuery(query, null);
         String a;
         String b;
-        b="Not found";
-        if(cursor.moveToFirst())
-        {
+        b = "Not found";
+        if (cursor.moveToFirst()) {
             do {
                 a = cursor.getString(0);
 
@@ -112,17 +113,85 @@ public class DBHelper extends SQLiteOpenHelper {
                     break;
                 }
             }
-                while(cursor.moveToNext());
-            }
-            return b;
+            while (cursor.moveToNext());
+        }
+        return b;
 
 
     }
 
 
-    public Cursor getAllData(){
+    public Cursor getAllData() {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor res = db.rawQuery("Select * from "+MAIN_TABLE,null);
+        Cursor res = db.rawQuery("Select * from " + MAIN_TABLE, null);
         return res;
+    }
+
+    public static final String c1 = "Day";
+    public static final String c3 = "Burnt";
+    public static final String c4 = "Total";
+    public static final String c2 = "Consumed";
+
+    boolean createUserTable(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("create table " + id +
+                " (Day INTEGER PRIMARY KEY ,Consumed REAL " +
+                ", Burnt REAL " +
+                ", Total REAL )");
+        ContentValues cV = new ContentValues();
+        cV.put(c1, 1);
+        cV.put(c2, 0);
+        cV.put(c3, 0);
+        cV.put(c4, 0);
+        return true;
+    }
+
+
+    int getCaloriesBurnt(int id) {
+
+        Integer i = new Integer(id);
+        SQLiteDatabase db = this.getReadableDatabase();
+        String str = "SELECT * FROM " + id + " WHERE DAY = 1";
+        Cursor c = db.rawQuery(str, null);
+        if (c != null)
+            c.moveToFirst();
+        return Integer.parseInt(c.getString(2));
+    }
+
+
+    int getCaloriesConsumed(int id) {
+        Integer i = new Integer(id);
+        SQLiteDatabase db = this.getReadableDatabase();
+        String str = "SELECT * FROM " + id + " WHERE DAY = 1";
+        Cursor c = db.rawQuery(str, null);
+        if (c != null)
+            c.moveToFirst();
+        return Integer.parseInt(c.getString(1));
+    }
+
+    boolean setCalories(int id,float calb,float calc)
+    {
+        Integer i = new Integer(id);
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(c2,calc);
+        cv.put(c3,calb);
+        db.update(i.toString(),cv,"day = 1",null);
+        //String str = "UPDATE "+ id +" SET "+ c3 +" = "+cal+" WHERE DAY = 1";
+        //db.execSQL(str);
+        return true;
+    }
+
+    float wtInPound() {
+        //SQLiteDatabase db = new SQLiteDatabase(this);
+        SQLiteDatabase db = this.getReadableDatabase();
+        String str = "SELECT * FROM "+MAIN_TABLE+" WHERE ID=1";
+        Cursor c = db.rawQuery(str, null);
+        if (c != null)
+            c.moveToFirst();
+        float wt =  Float.parseFloat(c.getString(5));
+        wt = wt * 2.204f;
+        return wt;
+
     }
 }
--- app/src/main/java/com/example/hemal/hms/HomePage.java
+++ app/src/main/java/com/example/hemal/hms/HomePage.java
@@ -1,13 +1,264 @@
 package com.example.hemal.hms;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.support.v4.app.NotificationCompat;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import java.lang.*;
 
-public class HomePage extends AppCompatActivity {
+
+public class HomePage extends AppCompatActivity implements SensorEventListener, StepListener {
+    Context context;
+    private TextView textView;
+    private StepDetector simpleStepDetector;
+    private SensorManager sensorManager;
+    private Sensor accel;
+    DBHelper myDB = new DBHelper(this);
+    private static final String TEXT_NUM_STEPS = "Number of Steps: ";
+    private static final String TEXT_DISTANCE = "total distacnce (in km):";
+    private static final String TEXT_SPEED = "Speed(in kmph):";
+
+    private static final String TEMP_TEXT_SPEED = " temp Speed(in kmph):";
+    private static final String TEMP_TEXT_TIME = " temp time :";
+    private static final String TEMP_TEXT_DIST = "temp dist (in km):";
+
+    private int numSteps;
+    private float distance, d, speed, s;
+    private TextView TvSteps, TvDistance, TvSpeed, TvCalories;
+    private TextView TempTvspeed, TempTvDistance, TempTvintervalOnPause;
+    private Button BtnStart, BtnStop, BtnPause;
+    private Chronometer sChronometer;
+    private long sLastStopTime = 0;
+    //private long tspeed,tdistance,tintervalOnPause;
+    //String Ntime[];
+
+    private float Hour = 3600;
+    private float Min = 60;
+    private float Second = 1;
+    private int Day = 3600 * 24;
+    private float TimeInSeconds = 0;
+    private long flag;
+    private CharSequence cs;
+    private String sex = "male";
+    HomePage instance = this;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.homepage);
+        //myDB.setCalories(1,100,1000);
+
+        //get an instance of location manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                location.getLatitude();
+                Toast.makeText(context, "Current speed:" + location.getSpeed(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+        //not working locationManager.requestLocationUpdates(LocationManager.GPS_PROV356cv  IDER, 0, 0, locationListener);
+
+
+        // Get an instance of the SensorManager
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        simpleStepDetector = new StepDetector();
+        simpleStepDetector.registerListener(this);
+
+
+        TvSpeed = (TextView) findViewById(R.id.tv_speed);
+        TvDistance = (TextView) findViewById(R.id.tv_distance);
+        TvSteps = (TextView) findViewById(R.id.tv_steps);
+        BtnStart = (Button) findViewById(R.id.btn_start);
+        BtnPause = (Button) findViewById(R.id.btn_pause);
+        BtnStop = (Button) findViewById(R.id.btn_stop);
+        sChronometer = (Chronometer) findViewById(R.id.chronometer2);
+        TvCalories = (TextView) findViewById(R.id.textView);
+
+
+        BtnStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // numSteps = 0;
+                sensorManager.registerListener(HomePage.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+                chronoStart();
+
+
+            }
+        });
+
+
+        BtnPause.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                sensorManager.unregisterListener(HomePage.this);
+                chronoPause();
+
+
+            }
+        });
+        BtnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                numSteps = 0;
+                sensorManager.registerListener(HomePage.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+                sLastStopTime = 0;
+                chronoStart();
+
+            }
+
+        });
+
+
     }
+
+
+    //CHRONOMETER
+    public void chronoStart() {
+
+        if (sLastStopTime == 0) {
+            sChronometer.setBase(SystemClock.elapsedRealtime());
+        } else {
+            long intervalOnPause = (SystemClock.elapsedRealtime() - sLastStopTime);
+
+            // TempTvintervalOnPause.setText(TEMP_TEXT_TIME+" --elapsed Time--"+intervalOnPause+"---slaststoptime-"+sLastStopTime+"---" +
+            //    "--getcd++"+sChronometer.getContentDescription());
+
+
+            sChronometer.setBase(sChronometer.getBase() + intervalOnPause);
+            sChronometer.getBase();
+        }
+        sChronometer.start();
+    }
+
+    public void chronoPause() {
+        sChronometer.stop();
+        sLastStopTime = SystemClock.elapsedRealtime();
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector.updateAccel(
+                    event.timestamp, event.values[0], event.values[1], event.values[2]);
+        }
+    }
+
+    @Override
+    public void step(long timeNs) {
+        numSteps++;
+        //calorie_count()
+        //estimated_calorie_burn()
+        //make_db_calls
+        double calburnt;
+        String calburnt1;
+        TvSteps.setText(TEXT_NUM_STEPS + numSteps);
+        d = getDistance(numSteps);
+
+
+        s = getSpeed(distance);
+        TvSpeed.setText(TEXT_SPEED + s + "kmph");
+        TvDistance.setText(TEXT_DISTANCE + d + "---Time--- " + TimeInSeconds);
+        float weight = myDB.wtInPound();
+        calburnt = (weight * 0.57 / 1.67)*d;
+        calburnt1 = new Double(calburnt).toString();
+        TvCalories.setText("Calories Burnt : "+ calburnt1 +" kcal");
+    }
+
+    //CONVERTING TIME FROM CHRONOMETER TO SECONDS TO CALCULATE SPEED
+    public float conTime() {
+        String tempString;
+        String contentDscString;
+        cs = sChronometer.getContentDescription();
+        contentDscString = cs.toString();
+        String tempDisplayTime[];
+        tempDisplayTime = contentDscString.split(" ");
+        Toast.makeText(instance, "time: " + tempDisplayTime[1], Toast.LENGTH_SHORT).show();
+        for (int i = 0; i < tempDisplayTime.length; i = i + 2) {
+            float tempNo = Float.parseFloat(tempDisplayTime[i]);
+            tempString = tempDisplayTime[i + 1];
+            if (tempDisplayTime[i + 1].contains("hour")) {
+                if (tempNo !=0)
+                TimeInSeconds += (tempNo * Hour)-TimeInSeconds;
+            }
+            if (tempDisplayTime[i + 1].contains("minute")) {
+                if (tempNo !=0)
+                TimeInSeconds += (tempNo * Min)-TimeInSeconds;
+            }
+            if (tempDisplayTime[i + 1].contains("seconds")) {
+                if (tempNo !=0)
+                TimeInSeconds += (tempNo * Second)-TimeInSeconds;
+            }
+
+        }
+        ;
+        return TimeInSeconds;
+
+    }
+
+    public float getDistance(int numSteps) {
+        if (sex.contains("male")) {
+            distance = (float) (numSteps * 78) / 100000;
+        } else {
+            distance = (float) (numSteps * 74) / 100000;
+        }
+        return distance;
+
+    }
+
+    public float getSpeed(float distance) {
+        //TempTvintervalOnPause.setText(TEMP_TEXT_TIME+sChronometer.getContentDescription());
+        CharSequence cs = sChronometer.getContentDescription();
+        // TempTvintervalOnPause.getText();
+        //int ntime=Integer.parseInt(cs.toString());
+
+        speed = ((distance) / (conTime())) * 3600;
+        // TempTvDistance.setText(TEMP_TEXT_SPEED+distance);
+        return speed;
+
+
+
+    }
+
+
+
+
 }
